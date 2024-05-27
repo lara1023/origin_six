8fdb99908e98d219e0e7c642390c5e60e6088127
lara1023 <larafgrcabral12@gmail.com>
2024-05-27 15:56:45 -0300

    15:56

--- src/js/script.js
+++ src/js/script.js
@@ -5,12 +5,12 @@ for (const element of toggle){
         nav.classList.toggle('show')
     })
 }
-//quando clicar em um item do menu, esconder o menu//
+// quando clicar em um item do menu, esconder o menu
 const links = document.querySelectorAll('nav ul li a')
-for (const link of links){
-        link.addEventListener('click', function(){
-            nav.classList.remove('show')
-        })
+for (const link of links) {
+    link.addEventListener('click', function (){
+        nav.classList.remove('show')
+    })
 }
 // Carrosel Swiper
 const swiper = new Swiper('.swiper-container',{
